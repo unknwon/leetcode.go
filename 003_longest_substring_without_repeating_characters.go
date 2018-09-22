8001786e8e113d120cbd7dbf6b7731a235a05b54
Unknwon <u@gogs.io>
2018-09-21 21:47:10 -0400

    003_longest_substring_without_repeating_characters

---
+++ 003_longest_substring_without_repeating_characters.go
@@ -0,0 +1,20 @@
+func lengthOfLongestSubstring(s string) int {
+	set := map[byte]bool{}
+	start := 0
+	end := 0
+	longest := 0
+	for start < len(s) && end < len(s) {
+		if !set[s[end]] {
+			set[s[end]] = true
+			end++
+			if end-start > longest {
+				longest = end - start
+			}
+			continue
+		}
+
+		delete(set, s[start])
+		start++
+	}
+	return longest
+}
