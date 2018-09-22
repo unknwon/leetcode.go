b1cadeef62b81b5ff6b1a22fc7eefe997100f534
Unknwon <u@gogs.io>
2018-09-22 02:14:49 -0400

    683_k_empty_slots

---
+++ 683_k_empty_slots.go
@@ -0,0 +1,32 @@
+func kEmptySlots(flowers []int, k int) int {
+	pots := make([]bool, len(flowers))
+
+LOOP:
+	for i := range flowers {
+		pot := flowers[i] // pot is 1-based
+		pots[pot-1] = true
+
+		// Left check
+		// 1 0 0 pot
+		if pot-k-1 > 0 && pots[pot-k-1-1] {
+			for j := pot - k; j < pot; j++ {
+				if pots[j-1] {
+					continue LOOP
+				}
+			}
+			return i + 1
+		}
+
+		// Right check
+		// pot 0 0 1
+		if pot+k+1 <= len(pots) && pots[pot+k] {
+			for j := pot + 1; j < pot+k+1; j++ {
+				if pots[j-1] {
+					continue LOOP
+				}
+			}
+			return i + 1
+		}
+	}
+	return -1
+}
